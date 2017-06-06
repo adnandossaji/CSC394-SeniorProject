fec6643579b77e84dfbd5afc246d1e2818ab0670
Adnan Dossaji <nan.dossaji@gmail.com>
2017-06-06 18:37:56 -0500

    changes

--- dump.sql
+++ dump.sql
@@ -1,4 +1,38 @@
-
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE alembic_version (
+	version_num VARCHAR(32) NOT NULL, 
+	CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
+);
+INSERT INTO "alembic_version" VALUES('cb1dd35af8df');
+CREATE TABLE user_role (
+	id INTEGER NOT NULL, 
+	name VARCHAR(120) NOT NULL, 
+	PRIMARY KEY (id), 
+	UNIQUE (id)
+);
+INSERT INTO "user_role" VALUES(1,'Admin');
+INSERT INTO "user_role" VALUES(2,'Faculty');
+INSERT INTO "user_role" VALUES(3,'Student');
+CREATE TABLE course_type (
+	id INTEGER NOT NULL, 
+	name VARCHAR(120) NOT NULL, 
+	PRIMARY KEY (id), 
+	UNIQUE (id)
+);
+CREATE TABLE course (
+	id INTEGER NOT NULL, 
+	subject VARCHAR(100) NOT NULL, 
+	course_number INTEGER NOT NULL, 
+	prereq VARCHAR(400) NOT NULL, 
+	credits INTEGER NOT NULL, 
+	day_of_week VARCHAR(100), 
+	description VARCHAR(700), 
+	quarter_offered VARCHAR(100) NOT NULL, 
+	delivery_method INTEGER NOT NULL, 
+	PRIMARY KEY (id), 
+	UNIQUE (id)
+);
 INSERT INTO "course" VALUES(1,'CSC',423,'IT 403',4,'Sun','','[''Not Offered'']',1);
 INSERT INTO "course" VALUES(2,'CSC',424,'CSC 423 or consent of instructor.',4,'Sun','','[''Not Offered'']',1);
 INSERT INTO "course" VALUES(3,'CSC',426,'PhD status or consent of instructor.',4,'Sat','','[''EO Spring'']',0);
@@ -299,3 +333,64 @@ INSERT INTO "course" VALUES(297,'GAM',576,'GAM 575',4,'Fri','','[''Not Offered''
 INSERT INTO "course" VALUES(298,'IS',550,'CSC 451 or CSC 453 or CSC 455',4,'Fri','','[''Not Offered'']',1);
 INSERT INTO "course" VALUES(299,'SE',441,'CSC 403',4,'Thu','','[''Not Offered'']',0);
 INSERT INTO "course" VALUES(300,'HCI',522,'[]',4,'Fri','','[''Not Offered'']',0);
+CREATE TABLE term (
+	id INTEGER NOT NULL, 
+	name VARCHAR(120) NOT NULL, 
+	year INTEGER NOT NULL, 
+	offered VARCHAR(120) NOT NULL, 
+	PRIMARY KEY (id, year), 
+	UNIQUE (id), 
+	UNIQUE (year)
+);
+CREATE TABLE user (
+	id INTEGER NOT NULL, 
+	name VARCHAR(120) NOT NULL, 
+	email VARCHAR(120) NOT NULL, 
+	password VARCHAR(30) NOT NULL, 
+	role_id INTEGER NOT NULL, 
+	active INTEGER NOT NULL, 
+	program VARCHAR(120) NOT NULL, 
+	concentration VARCHAR(120) NOT NULL, 
+	start_term VARCHAR(10) NOT NULL, 
+	start_year INTEGER NOT NULL, 
+	delivery_type INTEGER NOT NULL, 
+	classes_per_term INTEGER NOT NULL, 
+	taken VARCHAR(120) NOT NULL, 
+	PRIMARY KEY (id), 
+	UNIQUE (id), 
+	UNIQUE (email), 
+	FOREIGN KEY(role_id) REFERENCES user_role (id)
+);
+INSERT INTO "user" VALUES(1,'admin','admin@gmail.com','102137',1,0,'Information Systems','Software and Systems Development','Autumn',2017,'In-Class Only',1,'{}');
+INSERT INTO "user" VALUES(2,'stutofac','stutofac@gmail.com','102137',3,0,'Information Systems','Software and Systems Development','Autumn',2017,'In-Class Only',1,'{}');
+CREATE TABLE "Users" (
+	id INTEGER NOT NULL, 
+	name VARCHAR(120) NOT NULL, 
+	email VARCHAR(120) NOT NULL, 
+	password VARCHAR(30) NOT NULL, 
+	role INTEGER NOT NULL, 
+	active INTEGER NOT NULL, 
+	program VARCHAR(120) NOT NULL, 
+	concentration VARCHAR(120) NOT NULL, 
+	start_term VARCHAR(10) NOT NULL, 
+	start_year INTEGER NOT NULL, 
+	delivery_type INTEGER NOT NULL, 
+	classes_per_term INTEGER NOT NULL, 
+	taken VARCHAR(120) NOT NULL, 
+	PRIMARY KEY (id), 
+	UNIQUE (id), 
+	UNIQUE (email)
+);
+CREATE TABLE "Role" (
+	id INTEGER NOT NULL, 
+	name VARCHAR(120) NOT NULL, 
+	PRIMARY KEY (id), 
+	UNIQUE (id)
+);
+CREATE TABLE "CourseType" (
+	id INTEGER NOT NULL, 
+	name VARCHAR(120) NOT NULL, 
+	PRIMARY KEY (id), 
+	UNIQUE (id)
+);
+COMMIT;
--- manage_db.py
+++ manage_db.py
@@ -3,12 +3,12 @@ import sqlite3
 def main():
     conn = sqlite3.connect('database.db')
 
-    # sql_file = open("Schema.sql", "r").read()
+    sql_file = open("dump.sql", "r").read()
 
-    # try:
-    #     conn.executescript(sql_file)
-    # except Exception as e:
-    #     print(e)
+    try:
+        conn.executescript(sql_file)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
